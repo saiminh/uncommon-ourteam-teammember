353e74017129bae322a0674ec7fb4bc1577b1295
saiminh <saiminh@gmail.com>
2023-05-22 22:30:23 +0200

    load -> domcontentloaded

--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '4a260bcf206d2cb81a7a');
--- src/view.js
+++ src/view.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', function() {
   const ourteam = document.querySelector('.uncommon-ourteam'),
         switcher = document.querySelector('.uncommon-ourteam-switcher');
   switcher.addEventListener('click', ()=> {
